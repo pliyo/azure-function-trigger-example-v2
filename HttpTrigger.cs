71f9ea170ce2b0382577647bedacf0ee3bf98087
Juanjo Cerezuela <cerezuelaj@GLAdmins-MacBook-Pro.local>
2018-06-05 11:29:30 +0100

    Changing code to productId

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -6,25 +6,27 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Microsoft.Extensions.Primitives;
+using System.Linq;
 
 namespace Company.Function
 {
     public static class HttpTrigger
     {
         [FunctionName("HttpTrigger")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            if (req.Query.TryGetValue("productid", out StringValues value)) {
+                return new OkObjectResult($"The product name for your product id {value.First()} is Starfruit Explosion and the description is This starfruit ice cream is out of this world!");
+            }
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new BadRequestObjectResult("Please pass a productid on the query string or in the request body");
         }
     }
 }
